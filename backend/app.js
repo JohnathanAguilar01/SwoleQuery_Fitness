ccc61705cf49b7642d712a2d2dbce1e02ba58feb
Johnthan Aguilar <johnathan.aguilar.350@my.csun.edu>
2025-03-11 19:01:56 -0700

    made rouest for food, progress, and excersises

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,24 @@
 import express from "express";
 import cors from "cors";
-import mysql from "mysql2";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser"; // Import cookie-parser
+import foodItemsRoutes from "./routes/food_items.js"; // Import the routes
+
 dotenv.config();
 
 const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000", // Replace with your frontend's origin
+    origin: true, // Replace with your frontend's origin
     credentials: true, // Allow cookies and credentials
   }),
 );
+app.use(cookieParser()); // Use cookie-parser separately
 app.use(express.json());
 
-// Middleware to use Vehicles routes
-// const VehiclesRoutes = require("./routes/Vehicles.js");
-// app.use("/Vehicles", VehiclesRoutes);
+// Middleware to use food items routes
+app.use("/food_items", foodItemsRoutes);
 
 app.get("/", (req, res) => {
   res.send("Backend is running!");
