6b9ae7083c7dbaab3c6d9a1381828be41fd8a715
Johnthan Aguilar <johnathan.aguilar.350@my.csun.edu>
2025-03-12 22:59:32 -0700

    merged from johnny_dev

--- database/seeds/02_progress.js
+++ database/seeds/02_progress.js
@@ -3,6 +3,7 @@ exports.seed = async function (knex) {
   await knex("progress").insert([
     {
       user_id: 1, // Ensure a valid user_id is provided
+      recorded_at: "2025-03-12 11:25:46.00",
       weight: 120.6, // Example field
     },
   ]);
