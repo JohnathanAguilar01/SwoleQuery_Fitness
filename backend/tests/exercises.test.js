b8cbf510d548f483237705de446d30b0f2fa19ae
denver <cudedenver@gmail.com>
2025-03-28 16:45:19 -0700

    Complete 3 tests for exercises endpoint

--- backend/tests/exercises.test.js
+++ backend/tests/exercises.test.js
@@ -3,7 +3,7 @@ import app from "../app";
 
 describe("POST /add", () => {
     describe("given working data for all fields, cardio", () => {
-        test("should successfully add an exercise", async () => {
+        test("should successfully add an exercise, cardio", async () => {
           const response = await request(app).post("/exercises/add").send({
                 "user_id": 1,
                 "workout_id": 9,
@@ -13,6 +13,23 @@ describe("POST /add", () => {
                 "exercise_time": 30
             })
             .set("Content-Type", "application/json")
+            .expect(200);
+        });
+    });
+    
+    describe("given working data for all fields, strength", () => {
+        test("should successfully add an exercise, strength", async () => {
+            const response = await request(app).post("/exercises/add").send({
+                "user_id": 1,
+                "workout_id": 9,
+                "intensity": 25.00,
+                "exercise_type": "strength training",
+                "calories_burned": 250,
+                "weight": 100.00,
+                "sets" : 5,
+                "reps" : 5
+            })
+            .set("content-Type", "application/json")
             .expect(200);
         });
     });
