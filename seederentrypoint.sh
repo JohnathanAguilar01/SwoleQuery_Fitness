2518d0b169ec2568d8cbe87c569af939736ef647
Johnathan Aguilar <johnathan.aguilar.350@my.csun.edu>
2025-03-31 22:06:29 -0700

    Fixed it so the database dose not presist after the container is shutdown

--- seederentrypoint.sh
+++ seederentrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 # Check if the seeds have already been run
-if [ ! -f /app/database/.seeds_run ]; then
+# if [ ! -f /app/database/.seeds_run ]; then
   echo "Running knex seeds..."
   npx knex seed:run --knexfile /app/database/knexfile.js
   # Create a marker file to indicate that seeds have been executed
   touch /app/database/.seeds_run
-else
-  echo "Seeds already run. Skipping seeding step."
-fi
+# else
+#   echo "Seeds already run. Skipping seeding step."
+# fi
 
 # Optionally, start the application (or exit if this is a one-off seeder)
 # npm start
