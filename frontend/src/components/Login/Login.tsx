d5d6cc6f9da2f259031ad9b9ea0bf0eb601f8368
Johnathan Aguilar <johnathan.aguilar.350@my.csun.edu>
2025-04-04 01:55:16 -0700

    finished the design for the login window for the frontend

--- frontend/src/components/Login/Login.tsx
+++ frontend/src/components/Login/Login.tsx
@@ -1,16 +1,33 @@
 import { Input } from "@/components/ui/input";
 import { FaRunning } from "react-icons/fa";
+import { Button } from "@/components/ui/button";
 
 export default function Login() {
   return (
     <>
       <div className="flex justify-center items-center w-screen h-screen bg-zinc-200">
-        <div className="w-lg h-200 bg-white">
-          <div className="flex justify-center items-center mt-2 mb-8">
+        <div className="flex flex-col items-center w-lg bg-white rounded-xl p-12">
+          <div className="flex justify-center items-center mt-2 mb-14">
             <FaRunning size={60} />
             <h1 className="inline font-bold text-4xl">Swole</h1>
             <h1 className="inline font-bold text-4xl text-blue-500">Query</h1>
           </div>
+          <h1 className="text-center text-3xl font-semibold mb-4">LOGIN</h1>
+          <Input
+            type="username"
+            id="username"
+            placeholder="Username"
+            className="w-96 mx-auto my-4"
+          />
+          <Input
+            type="password"
+            id="password"
+            placeholder="Password"
+            className="w-96 mx-auto my-4"
+          />
+          <Button variant="outline" className="w-36">
+            Login
+          </Button>
         </div>
       </div>
     </>
