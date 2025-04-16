455a3ce2cbb7cc23aca69c44bac29aca2db84641
denver <cudedenver@gmail.com>
2025-04-15 19:55:56 -0700

    Fix app.tsx back to normal

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,12 @@ import { useUser } from "./context/UserContext";
 import MainPage from "./components/MainPage/MainPage";
 import DashBoard from "./components/MainPage/Dashboard";
 import Login from "./components/Login/Login";
-import Signup from "./components/Signup/Signup";
 
 function App() {
   const { user } = useUser();
 
   if (!user) {
-    return <Signup />;
+    return <Login />;
   }
   return (
     <>
