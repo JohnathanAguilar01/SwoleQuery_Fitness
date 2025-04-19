ec3121b8b608cefa0792bb142a8da1a96e17961f
Johnathan Aguilar <johnathan.aguilar.350@my.csun.edu>
2025-04-19 14:00:21 -0700

    Made it so you can switch between tabs and added the logs tab

--- frontend/src/components/AuthRoutes/AuthRoutes.tsx
+++ frontend/src/components/AuthRoutes/AuthRoutes.tsx
@@ -1,18 +1,26 @@
+import { useState } from "react";
 import { useUser } from "@/context/UserContext";
 import { Navigate } from "react-router-dom";
 import MainPage from "@/components/MainPage/MainPage";
 import DashBoard from "@/components/MainPage/Dashboard";
+import Logs from "@/components/MainPage/Logs";
+
+const TABS = {
+  dashboard: <DashBoard />,
+  logs: <Logs />,
+};
 
 export default function AuthRoutes() {
   const { user } = useUser();
+  const [activeTab, setActiveTab] = useState<keyof typeof TABS>("dashboard");
 
   if (!user) {
     return <Navigate to="/login" replace />;
   }
 
   return (
-    <MainPage>
-      <DashBoard />
+    <MainPage setActiveTab={setActiveTab} activeTab={activeTab}>
+      {TABS[activeTab]}
     </MainPage>
   );
 }
